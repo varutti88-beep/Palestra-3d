fac786db53855e985c0554a507c524efa54ea75f
Gabri <varutti88@mgmail.com>
2026-03-05 16:53:57 +0100

    force /m to MobileApp

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
+import MobileApp from "./MobileApp.jsx";
+
+const isMobileRoute = window.location.pathname.startsWith("/m");
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      {isMobileRoute ? <MobileApp /> : <App />}
     </BrowserRouter>
   </React.StrictMode>
 );
